50277e852b5643675a7d54de57526631f2b6b795
Mahmoud Ismail <mahmoudismail9114@gmail.com>
2025-11-22 17:19:27 +0200

    feat: add endpoint to create single employee feat: add endpoint to delete single employee by ID feat: add endpoint to update single employee by ID

--- src/main/java/com/System/Employee_Management_System/controllers/EmployeeControllers.java
+++ src/main/java/com/System/Employee_Management_System/controllers/EmployeeControllers.java
@@ -1,10 +1,7 @@
 package com.System.Employee_Management_System.controllers;
 
 import com.System.Employee_Management_System.entities.Employee;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -42,14 +39,48 @@ public class EmployeeControllers {
     );
 
     @GetMapping("")
-    public ArrayList<Employee> getEmployees(){
+    public ArrayList<Employee> getEmployees() {
         return employees;
     }
 
     @GetMapping("/{employeeID}")
-    public Optional<Employee> getEmployeeByID(@PathVariable UUID employeeID){
+    public Optional<Employee> getEmployeeByID(@PathVariable UUID employeeID) {
         return employees.stream()
                 .filter(emp -> emp.getId().equals(employeeID))
                 .findFirst();
     }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        employee.setId(UUID.randomUUID());
+        employee.setDepartmentId(UUID.randomUUID());
+        employees.add(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/{employeeID}")
+    public Optional<Employee> deleteEmployee(@PathVariable UUID employeeID) {
+        Optional<Employee> employee = employees.stream()
+                .filter(emp -> emp.getId().equals(employeeID))
+                .findFirst();
+        employee.ifPresent(emp -> employees.remove(emp));
+        return employee;
+    }
+
+    @PutMapping("/{employeeID}")
+    public Employee updateEmployee(@PathVariable UUID employeeID, @RequestBody Employee employee) {
+        Optional<Employee> existingemployee = employees.stream()
+                .filter(emp -> emp.getId().equals(employeeID))
+                .findFirst();
+        if (existingemployee.isPresent()) {
+            existingemployee.get().setFristName(employee.getFristName());
+            existingemployee.get().setLastName(employee.getLastName());
+            existingemployee.get().setEmail(employee.getEmail());
+            existingemployee.get().setPhoneNumber(employee.getPhoneNumber());
+            existingemployee.get().setHireDate(employee.getHireDate());
+            existingemployee.get().setPosition(employee.getPosition());
+        }
+        return employee;
+    }
+
 }
